18e8da29a83b41211cc21d2ad6a6af27bbef5e09
Abdelrahman Farid <abdelrahman.farid99@eng-st.cu.edu.eg>
2021-03-01 01:47:14 +0200

    Template cleanup

--- stress-test.hpp
+++ stress-test.hpp
@@ -1,5 +1,5 @@
-#include <random>
 #include <functional>
+#include <random>
 
 /**
  * @todo[XL3]: Add a wrapper that does trials for you
@@ -8,26 +8,42 @@
  * @todo[XL3]: Add specialization for vector types
  */
 namespace Stress {
-  template <typename Ty_dst>
-  class discrete_parameter {
-    static std::random_device rd;
-    static std::mt19937 mersenneEngine;
+template <typename Ty_dst>
+class discrete_parameter {
+  inline static std::random_device rd{};
+  inline static std::mt19937 mersenneEngine{rd()};
 
-    Ty_dst min;
-    Ty_dst max;
-    std::uniform_int_distribution<Ty_dst> distribution;
+  Ty_dst min;
+  Ty_dst max;
+  std::uniform_int_distribution<Ty_dst> distribution;
 
-  public:
-    discrete_parameter(Ty_dst _min, Ty_dst _max);
-    operator Ty_dst() const;
+ public:
+  std::function<Ty_dst()> f_gen;
 
-    const Ty_dst generate() const;
-    std::function<Ty_dst()> f_gen;
+  discrete_parameter(Ty_dst _min, Ty_dst _max) : distribution(_min, _max) {
+    min = _min, max = _max;
+    f_gen = [this]() { return distribution(mersenneEngine); };
+  }
 
-    template <typename Ty_itr>
-    void fill_range(Ty_itr _begin, Ty_itr _end);
+  operator Ty_dst() const {
+    return distribution(mersenneEngine);
+  }
 
-    void fill_range(std::vector<Ty_dst> &_vec, int _L, int _R);
-    void fill_vector(std::vector<Ty_dst> &_vec);
-  };
-}
+  const Ty_dst generate() const {
+    return distribution(mersenneEngine);
+  }
+
+  template <typename Ty_itr>
+  void fill_range(Ty_itr _begin, Ty_itr _end) {
+    std::generate(_begin, _end, f_gen);
+  }
+
+  void fill_range(std::vector<Ty_dst> &_vec, int _L, int _R) {
+    std::generate(_vec.begin() + _L, _vec.begin() + _R, f_gen);
+  }
+
+  void fill_vector(std::vector<Ty_dst> &_vec) {
+    std::generate(_vec.begin(), _vec.end(), f_gen);
+  }
+};
+}  // namespace Stress
